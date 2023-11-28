ea7572af5d998100f62f9a6e6156cbcceafba283
[dnqls2446] <[dnqls9875@gmail.com]>
2023-11-28 18:03:43 +0900

    복습

--- 1_basics/13_must_know_array_functions_practice.js
+++ 1_basics/13_must_know_array_functions_practice.js
@@ -4,58 +4,58 @@ console.log(familyMembers);
 console.log("=====================");
 
 // push()
-console.log(familyMembers.push("또복"));
+console.log(familyMembers.push("또복")); // 뒤에 추가
 console.log(familyMembers);
 console.log("=====================");
 
 // pop()
-console.log(familyMembers.pop());
+console.log(familyMembers.pop()); // 뒤에 삭제
 console.log(familyMembers);
 console.log("=====================");
 
 // shift()
-console.log(familyMembers.shift());
+console.log(familyMembers.shift()); // 앞에 삭제
 console.log(familyMembers);
 console.log("=====================");
 
 // unshift()
-console.log(familyMembers.unshift("또복"));
+console.log(familyMembers.unshift("또복")); // 앞에 추가
 console.log(familyMembers);
-console.log(familyMembers.unshift("아빠"));
+console.log(familyMembers.unshift("아빠")); // 앞에 추가
 console.log(familyMembers);
 console.log("=====================");
 
 // splice()
-console.log(familyMembers.splice(0, 3));
-console.log(familyMembers);
+console.log(familyMembers.splice(0, 3)); // 0부터 3개까지의 인덱스 자르기 -> [ '아빠', '또복', '엄마' ]
+console.log(familyMembers); // [ '누나', '소현', '우빈', '쁘니', '리온' ]
 console.log("=====================");
 
 familyMembers = ["아빠", "엄마", "누나", "소현", "우빈", "쁘니", "리온"];
 
 // concat()
-console.log(familyMembers.concat("또복"));
+console.log(familyMembers.concat("또복")); // push와 같지만 기존 배열 영향주지 않음
 console.log(familyMembers);
 console.log("=====================");
 
 // slice()
-console.log(familyMembers.slice(0, 4));
-console.log(familyMembers);
+console.log(familyMembers.slice(0, 4)); // 0부터 4전인 3까지의 인덱스 자르기 -> [ '아빠', '엄마', '누나', '소현' ]
+console.log(familyMembers); // 기존 배열 영향 주지 않음
 console.log("=====================");
 
 // spread operator
-let familyMembers2 = [...familyMembers];
+let familyMembers2 = [...familyMembers]; // ["아빠", "엄마", "누나", "소현", "우빈", "쁘니", "리온"];
 console.log(familyMembers);
 console.log("=====================");
 
 // join
-console.log(familyMembers.join());
-console.log(familyMembers.join("❤"));
+console.log(familyMembers.join()); // 아빠,엄마,누나,소현,우빈,쁘니,리온
+console.log(familyMembers.join("❤")); // 아빠❤엄마❤누나❤소현❤우빈❤쁘니❤리온
 console.log("=====================");
 
 // sort()
-familyMembers.sort();
+familyMembers.sort(); // 오름차순 정렬
 console.log(familyMembers);
-console.log(familyMembers.reverse());
+console.log(familyMembers.reverse()); // 내림차순 정렬
 console.log("=====================");
 
 let numbers = [1, 8, 6, 9, 5, 4];
@@ -84,15 +84,15 @@ console.log("=====================");
 // filter
 numbers = [1, 8, 6, 9, 5, 4]; // 33
 
-console.log(numbers.filter((x) => x % 2 === 0));
+console.log(numbers.filter((x) => x % 2 === 0)); // [8,6,4]
 console.log("=====================");
 
 // find()
-console.log(numbers.find((x) => x % 2 === 0));
+console.log(numbers.find((x) => x % 2 === 0)); // 8
 console.log("=====================");
 
 // findIndex()
-console.log(numbers.findIndex((x) => x % 2 === 0));
+console.log(numbers.findIndex((x) => x % 2 === 0)); // 1
 console.log("=====================");
 
 console.log(numbers.reduce((p, n) => p + n, 0));
